f7110ec34da0227b8c72578dab2c0c137e70fe2a
OG-1534 <gershomochanda@gmail.com>
2024-06-26 21:26:32 +0000

    Using the function prototype below

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+/* disable-eslint, no console */
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
+}

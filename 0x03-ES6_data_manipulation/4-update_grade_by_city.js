c00f711a16dcde365f0f0475d7d24f1158b80129
OG-1534 <gershomochanda@gmail.com>
2024-07-02 23:45:09 +0300

    Create a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade. It should accept a list of students (from getListStudents), a city (String), and newGrades (Array of “grade” objects) as parameters. newGrades is an array of objects with this format: {studentId: 31, grade: 78,}

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-prototype-builtins */
+export default function updateStudentGradeByCity(list, city, newGrades) {
+  return list
+    .filter((obj) => obj.location === city)
+    .map((student) => {
+      //   console.log(newGrades);
+      newGrades.map((studentGrade) => {
+        if (studentGrade.studentId === student.id) {
+          // eslint-disable-next-line no-param-reassign
+          student.grade = studentGrade.grade;
+        }
+
+        if (!student.hasOwnProperty('grade')) {
+          student.grade = 'N/A';
+        }
+        return student;
+      });
+
+      return student;
+    });
+}

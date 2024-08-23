41c724167f0d204b142c54496f404142032298d2
OG-1534 <gershomochanda@gmail.com>
2024-08-24 00:15:31 +0300

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,66 +1,63 @@
-// 7-http_express.js
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
+
+const { readFile } = require('fs');
 
 const app = express();
-const PORT = 1245;
+const port = 1245;
 
-const parseCSV = async (filePath) => {
-  const students = { CS: [], SWE: [] };
-  const fileStream = fs.createReadStream(filePath);
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  });
-
-  for await (const line of rl) {
-    const [name, field] = line.split(',');
-    if (name && field) {
-      if (field === 'CS') {
-        students.CS.push(name);
-      } else if (field === 'SWE') {
-        students.SWE.push(name);
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
       }
-    }
-  }
-
-  return students;
-};
+    });
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
-
-app.get('/students', async (req, res) => {
-  const filePath = process.argv[2];
-
-  if (!filePath) {
-    return res.status(400).send('Database file is missing');
-  }
-
-  try {
-    const students = await parseCSV(filePath);
-
-    const csStudents = students.CS.join(', ');
-    const sweStudents = students.SWE.join(', ');
-    const totalCS = students.CS.length;
-    const totalSWE = students.SWE.length;
-
-    res.send(
-      `This is the list of our students\n` +
-      `Number of students: ${totalCS + totalSWE}\n` +
-      `Number of students in CS: ${totalCS}. List: ${csStudents}\n` +
-      `Number of students in SWE: ${totalSWE}. List: ${sweStudents}`
-    );
-  } catch (error) {
-    res.status(500).send('Error reading the database file');
-  }
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2].toString()).then((output) => {
+    res.send(['This is the list of our students', output].join('\n'));
+  }).catch(() => {
+    res.send('This is the list of our students\nCannot load the database');
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.listen(port, () => {
 });
 
 module.exports = app;
